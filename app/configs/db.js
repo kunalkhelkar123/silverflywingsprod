7b5033e5b78b278ed512e28e69aef83656be5dbb
Kunal Khelkar <132210427+kunalkhelkar123@users.noreply.github.com>
2024-12-07 22:05:00 +0530

    Update db.js

--- app/configs/db.js
+++ app/configs/db.js
@@ -1,36 +1,3 @@
-// import mysql from 'mysql2/promise';
-
-// const connection = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_PORT,
-//     ssl: { rejectUnauthorized: false }, // Ensures SSL is used as required by DigitalOcean
-// });
-
-// // Test the connection
-// async function testConnection() {
-//     try {
-//         const conn = await connection.getConnection();
-//         console.log("Connected to the database successfully!");
-//         conn.release(); // Release the connection back to the pool
-//     } catch (error) {
-//         console.error("Failed to connect to the database:", error.message);
-//         console.error("Stack Trace:", error.stack);
-//     }
-// }
-
-// // Call the function to test the connection
-// testConnection();
-
-// export default connection;
-
-
-
-
-
-
 import mysql from 'mysql2/promise';
 
 const connection = mysql.createPool({
@@ -58,3 +25,36 @@ async function testConnection() {
 testConnection();
 
 export default connection;
+
+
+
+
+
+
+// import mysql from 'mysql2/promise';
+
+// const connection = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT,
+//     ssl: { rejectUnauthorized: false }, // Ensures SSL is used as required by DigitalOcean
+// });
+
+// // Test the connection
+// async function testConnection() {
+//     try {
+//         const conn = await connection.getConnection();
+//         console.log("Connected to the database successfully!");
+//         conn.release(); // Release the connection back to the pool
+//     } catch (error) {
+//         console.error("Failed to connect to the database:", error.message);
+//         console.error("Stack Trace:", error.stack);
+//     }
+// }
+
+// // Call the function to test the connection
+// testConnection();
+
+// export default connection;
